e1e4a9f891b8fbea463ff685d4c55fd5654842b4
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2023-04-19 09:39:33 -0500

    initial changes

---
+++ src/services/workoutService.js
@@ -0,0 +1,34 @@
+const Workout = require("../database/Workout");
+const getAllWorkouts = () => {
+
+  const allWorkouts = Workout.getAllWorkouts();
+
+  return allWorkouts;
+}
+// const getAllWorkouts = () => {
+//     return;
+//   };
+  
+  const getOneWorkout = () => {
+    return;
+  };
+  
+  const createNewWorkout = () => {
+    return;
+  };
+  
+  const updateOneWorkout = () => {
+    return;
+  };
+  
+  const deleteOneWorkout = () => {
+    return;
+  };
+  
+  module.exports = {
+    getAllWorkouts,
+    getOneWorkout,
+    createNewWorkout,
+    updateOneWorkout,
+    deleteOneWorkout,
+  };
